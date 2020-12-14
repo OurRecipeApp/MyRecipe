58d1fd0e689b22727d69270e62cd75a552ef9260
JMumford45 <JMumford45@gmail.com>
2020-12-13 21:22:04 -0800

    staticfiles(dir) added image

--- RecipeApp/settings.py
+++ RecipeApp/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_media")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 MEDIA_URL = "/media/"
 
